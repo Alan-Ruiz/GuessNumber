806ed44f6af5dca2c2508625935002aa37c7b01b
Alan Rodriguez <gheryk90@gmail.com>
2024-05-24 07:58:35 +0100

    timer starts count

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
     <>
       <Player />
       <div id="challenges">
-        <TimeChallenge title="Challenge 1" targetTime={5} />
-        <TimeChallenge title="Challenge 2" targetTime={10} />
-        <TimeChallenge title="Challenge 3" targetTime={15} />
-        <TimeChallenge title="Challenge 4" targetTime={20} />
+        <TimeChallenge title="Challenge 1" targetTime={1} />
+        <TimeChallenge title="Challenge 2" targetTime={3} />
+        <TimeChallenge title="Challenge 3" targetTime={5} />
+        <TimeChallenge title="Challenge 4" targetTime={10} />
       </div>
     </>
   );
--- src/components/TimeChallenge.jsx
+++ src/components/TimeChallenge.jsx
@@ -1,46 +1,28 @@
 import { useState } from 'react';
 export default function TimeChallenge({ title, targetTime }) {
-  const [buttonTitle, setButtonTitle] = useState("Start Challenge")
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
 
-  function handleClick() {
-    console.log(start)
-    if (buttonTitle === "Start Challenge") {
-      setButtonTitle("Stop")
-      let end = new Date().getTime();
-      let time = end - start;
-      console.log(time)
-    } else {
-      let start = new Date().getTime();
-      setButtonTitle("Start Challenge")
-    }
-  };
-
-  function startCount() {
-    setButtonTitle("Stop")
-    let start = new Date().getTime();
-    return start
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
   }
-
-  function running(buttonTitle) {
-    if (buttonTitle === "Start Challenge") {
-      false
-    } else {
-      true
-    }
-  };
 
   return <section className="challenge">
     <h2>{title}</h2>
+    {timerExpired && <p>You lost!</p> }
     <p className="challenge-time">
       {targetTime} second{targetTime > 1 ? 's' : ''}
     </p>
     <p>
-      <button onClick={handleClick}>
-        {buttonTitle}
+      <button onClick={handleStart}>
+        {timerStarted ? 'Stop' : 'Start'} Challenge
       </button>
     </p>
-    <p className="">
-      Time is running... / Timer inactive
+    <p className={timerStarted ? 'active' : undefined}>
+      {timerStarted ? 'Time is running...' : 'Timer inactive'}
     </p>
   </section>
 }
